f334098d1f7d1abbbd8cd05266d3e048cd418d2b
Ribhi El-Zaru <ribhielzaru@gmail.com>
2025-10-03 11:50:11 +0300

    placeholder

--- build.sbt
+++ build.sbt
@@ -3,17 +3,18 @@ ThisBuild / organization := "com.rezonation"
 ThisBuild / version      := "0.1.0-SNAPSHOT"
 ThisBuild / scalaVersion := "3.3.6"
 
+val commonDeps = Seq(
+      "dev.zio" %% "zio" % "2.0.21",
+      "dev.zio" %% "zio-json" % "0.7.44",
+      "dev.zio" %% "zio-kafka" % "3.1.0" 
+    )
+
 // Common Kafka Messages Project
 lazy val kafkaCommon = (project in file("kafka-common"))
   .settings(
     name := "kafka-common",
     scalaVersion := "3.3.6",
-    libraryDependencies ++= Seq(
-      "dev.zio" %% "zio" % "2.0.21",
-      "dev.zio" %% "zio-json" % "0.7.44",
-      "dev.zio" %% "zio-kafka" % "3.1.0" 
-
-    )
+    libraryDependencies ++= commonDeps
   )
 
 
@@ -24,30 +25,25 @@ lazy val httpServer = project
     name := "http-server",
     scalaVersion := "3.3.6",
     libraryDependencies ++= Seq(
-      "dev.zio" %% "zio" % "2.0.21",
       "dev.zio" %% "zio-http" % "3.5.1",
-      "dev.zio" %% "zio-kafka" % "3.1.0",
-      "dev.zio" %% "zio-json"    % "0.7.44",
       "dev.zio" %% "zio-config"          % "4.0.5",
       "dev.zio" %% "zio-config-magnolia" % "4.0.5",
       "dev.zio" %% "zio-config-typesafe" % "4.0.5",
       "dev.zio" %% "zio-config-refined"  % "4.0.5"
-    ),
-    Compile / run / mainClass := Some("com.rezonation.Main")
+    ) ++ commonDeps
   )
 
-// TODO use common
-lazy val kafkaConsumer = (project in file("kafka-consumer"))
+lazy val kafkaConsumer = project
   .dependsOn(kafkaCommon)
+  .in(file("kafka-consumer"))
   .settings(
     libraryDependencies ++= Seq(
-      "dev.zio" %% "zio" % "2.0.21",
       "dev.zio" %% "zio-http" % "3.5.1",
-      "dev.zio" %% "zio-kafka" % "3.1.0",
-      "dev.zio" %% "zio-json"    % "0.7.44",
       "dev.zio" %% "zio-config"          % "4.0.5",
       "dev.zio" %% "zio-config-magnolia" % "4.0.5",
       "dev.zio" %% "zio-config-typesafe" % "4.0.5",
-      "dev.zio" %% "zio-config-refined"  % "4.0.5"
-    ),
+      "dev.zio" %% "zio-config-refined"  % "4.0.5",
+      "net.ruippeixotog" %% "scala-scraper" % "3.2.0",
+      "net.dankito.readability4j" % "readability4j" % "1.0.8"
+    ) ++ commonDeps,
   )
--- kafka-consumer/src/main/scala/com/rezonation/services/ArticleIngester.scala
+++ kafka-consumer/src/main/scala/com/rezonation/services/ArticleIngester.scala
@@ -3,18 +3,57 @@ package com.rezonation.services
 import com.rezonation.types.events.ProcessArticleEvent
 import zio.ZIO
 import zio.ZLayer
-
+import net.dankito.readability4j.Readability4J
+import net.ruippeixotog.scalascraper.browser.JsoupBrowser
+import scala.io.Source
 class ArticleIngester() {
+  // val browser = JsoupBrowser()
 
+  // Highlevel TODO. Return the List of ArticleEntity to be stored in DB.
+  // The insertion will happen in another service.
   def ingestArticles(events: List[ProcessArticleEvent]): ZIO[Any, Throwable, Unit] = {
     // Placeholder for actual ingestion logic
     ZIO.mergeAllPar(events.map(ingestArticle))(())((_, _) => ())
   }
+  
+  // TODO Get tags. 
+  // 1. Extract Tags from URL
+  // 2. Enhance with own tags via NLP parsing.
+  // 3. Compose article to put into database. 
 
   private def ingestArticle(event: ProcessArticleEvent): ZIO[Any, Throwable, Unit] = {
     // Simulate ingestion logic, e.g., fetching and processing the article
-    ZIO.succeed(println(s"Processing article from URL: ${event.url}"))
+    for {
+      _      <- ZIO.succeed(println(s"Processing article from URL: ${event.url}"))
+      source  = Source.fromURL(event.url, "UTF-8").mkString
+      x       = new Readability4J(event.url, source)
+      article = x.parse()
+      _      <- ZIO.log(s"Title: ${article.getTitle}")
+      _      <- ZIO.log(s"Author: ${article.getByline}")
+      _      <- ZIO.log(s"Excerpt: ${article.getExcerpt}")
+      y = article.getContent()
+    } yield ()
   }
+  /*
+  import net.dankito.readability4j.Readability4J
+import java.net.URL
+import scala.io.Source
+
+// Fetch raw HTML (you can use scala.io.Source, sttp, or Jsoup)
+val url = "https://edition.cnn.com/2025/10/01/middleeast/analysis-trump-israel-gaza-peace-plan-latam-intl"
+val html = Source.fromURL(url, "UTF-8").mkString
+
+// Run Readability
+val readability = new Readability4J(new URL(url), html)
+val article = readability.parse()
+
+// Extract fields
+println(s"Title: ${article.getTitle}")
+println(s"Author: ${article.getByline}")     // may be empty if missing
+println(s"Excerpt: ${article.getExcerpt}")
+println(s"Content snippet: ${article.getContent.take(300)}...")
+
+   */
 }
 
 object ArticleIngester {
